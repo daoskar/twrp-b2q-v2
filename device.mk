68031ee16011b8b9da9d2187c917e9723572af43
Dawid <daoskar@gmail.com>
2026-09-16 20:33:31 +0200

    b2q: include Qualcomm FBE decryption helpers

--- device.mk
+++ device.mk
@@ -21,3 +21,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# Qualcomm FBE decryption helpers
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
