1021d661e386bb461ba1c7fdb7554f4b97ae38f8
Dawid <daoskar@gmail.com>
2026-09-22 19:39:57 +0200

    test6ab: remove duplicate SP loader make module

--- sp_loader/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := b2q-sp-loader
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := b2q-sp-loader.c
-LOCAL_SHARED_LIBRARIES := libdl
-LOCAL_CFLAGS += -Wall -Wextra -std=c11
-include $(BUILD_EXECUTABLE)
